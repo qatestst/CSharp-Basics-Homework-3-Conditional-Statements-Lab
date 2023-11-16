a122918a50c8e8223c1e2264dedd6b5ddea395d0
HP <HP@DESKTOP-ZMM>
2023-11-16 15:34:57 +0200

    Add project files.

---
+++ 04. Password Guess/04. Password Guess.cs
@@ -0,0 +1,19 @@
+﻿namespace _04._Password_Guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
---
+++ 07. Area of Figures/07. Area of Figures.cs
@@ -0,0 +1,33 @@
+﻿namespace _07._Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string figure =  Console.ReadLine();
+            if (figure == "square")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{sideA * sideA:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse((Console.ReadLine()));
+                double sideB = double.Parse((Console.ReadLine()));
+                Console.WriteLine($"{sideA * sideB:F3}");
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{Math.PI * radius * radius:F3}");
+            }
+            else if (figure == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double heightToSideA = double.Parse(Console.ReadLine());
+                Console.WriteLine(0.5*sideA*heightToSideA);
+            }
+        }
+    }
+}
